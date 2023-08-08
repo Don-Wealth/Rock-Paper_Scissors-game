c0d14eb0b2b3feafa5b900e1783c650d7d893a95
Modozie-Chika <modoziechikeluba6@gmail.com>
2023-08-08 03:07:54 +0100

    initial commit

--- Rock_Paper_Sicissors.py
+++ Rock_Paper_Sicissors.py
@@ -1,3 +1,47 @@
 import random
 
-def check_choice():
+
+def get_choices():
+    player_choice = input("Enter a choice (Rock, Paper or Scissors): ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"Player": player_choice, "Computer": computer_choice}
+    return choices
+
+def check_win(player, computer):
+    player = player.lower()
+    if player == computer:
+        return ("It's a tie!")
+    elif player == "rock":
+        if computer == "scissors":
+            return ("Rock smashes scissors!You win!")
+        else:
+            return ("Paper covers Rock!You lose.")
+    elif player == "paper":
+        if computer == "rock":
+            return ("Paper covers rock!You win!")
+        else:
+            return ("Scissors cuts paper!You lose.")
+    elif player == "scissors":
+        if computer == "paper":
+            return ("Scissors cuts paper!You win!")
+        else:
+            return ("Rock smashes scissors! You lose")
+
+def main():        
+    while True:       
+        choices = get_choices()
+        result = check_win(choices ["Player"], choices ["Computer"])
+        print (result)
+
+        while True:
+            replay = input("Do you want to play again? ").lower()
+            if replay == "yes":
+                break
+            elif replay == "no":
+                print("Bye!")
+                return
+            else:
+                print("Enter Yes or No!")
+
+main()
